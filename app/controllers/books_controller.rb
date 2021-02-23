5d3290579e547d81f52d140ff19010a380882a7f
fannyibanez <fannyibanez89@gmail.com>
2021-02-23 15:04:03 +0100

    book: CRUD only index/show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,16 @@
 class BooksController < ApplicationController
+  before_action :set_books, only: [:show]
+
   def index
+    @books = Book.all
   end
 
   def show
+  end
+
+  private
+
+  def set_books
+    @book = Book.find(params[:id])
   end
 end
