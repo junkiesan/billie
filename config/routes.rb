6303e13567fd31b41c231e1e1402d5569b26c31d
fannyibanez <fannyibanez89@gmail.com>
2021-02-23 15:06:38 +0100

    book: routes refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
+
   devise_for :users
   root to: 'pages#home'
+  resources :books, only: [:index, :show]
 
   get "/profile", to: "pages#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
