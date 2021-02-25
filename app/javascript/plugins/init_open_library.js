0d65d622020ea7b7b858a6f6177029ace2f340c5
fannyibanez <fannyibanez89@gmail.com>
2021-02-25 13:01:23 +0100

    API: fetch books OK

--- app/javascript/plugins/init_open_library.js
+++ app/javascript/plugins/init_open_library.js
@@ -1,7 +1,31 @@
+
+// const convertTitle = (query) => {
+//   fetch(`https://openlibrary.org/search.json?q=${query}`)
+//     .then(response => response.json())
+//     .then(insertBooks);
+//     console.log(data);
+// };
+
+const list = document.querySelector(".list-books");
+
+const insertBooks = (data) => {
+  data.docs.forEach((result) => {
+    const book = `<li>
+      <p>${result.title}</p>
+      <p>${result.author_name}</p>
+    </li>`;
+    list.insertAdjacentHTML('beforeend', book);
+  });
+};
+
+
 const fetchBooks = (query) => {
-  fetch(`https://openlibrary.org/`)
+  fetch(`https://openlibrary.org/search.json?q=naruto`)
     .then(response => response.json())
     .then(insertBooks);
 };
 
 export { fetchBooks };
+
+
+// https://openlibrary.org/search?title=${query}
