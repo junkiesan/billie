51d65b6e64576f16e06e424f9d5cddbd7c8cd38b
fannyibanez <fannyibanez89@gmail.com>
2021-02-26 14:44:46 +0100

    Tests: validations model book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
-  # validates :title, presence: true
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :description, presence: true
 end
