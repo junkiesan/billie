a0849a41e7f4ab946913299645d5002f0a0c4721
fannyibanez <fannyibanez89@gmail.com>
2021-03-24 15:36:59 +0100

    quagga.js : application.js quagga functions added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("quagga").start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -24,16 +25,72 @@ require("channels")
 
 // External imports
 import "bootstrap";
+
+// Internal imports, e.g:
 import {fetchBooks} from "../plugins/init_open_library";
 import {fetchGoogleBooks} from "../plugins/init_google_books";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+//Quagga functions
+function order_by_occurrence(arr) {
+  var counts = {};
+  arr.forEach(function(value){
+      if(!counts[value]) {
+          counts[value] = 0;
+      }
+      counts[value]++;
+  });
+
+  return Object.keys(counts).sort(function(curKey,nextKey) {
+      return counts[curKey] < counts[nextKey];
+  });
+}
+
+function load_quagga(){
+  if ($('#barcode-scanner').length > 0 && navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
+
+    var last_result = [];
+    if (Quagga.initialized == undefined) {
+      Quagga.onDetected(function(result) {
+        var last_code = result.codeResult.code;
+        last_result.push(last_code);
+        if (last_result.length > 20) {
+          code = order_by_occurrence(last_result)[0];
+          last_result = [];
+          Quagga.stop();
+          $.ajax({
+            type: "POST",
+            url: '/products/get_barcode',
+            data: { upc: code }
+          });
+        }
+      });
+    }
+
+    Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        numOfWorkers: navigator.hardwareConcurrency,
+        target: document.querySelector('#barcode-scanner')
+      },
+      decoder: {
+          readers : ['ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader']
+      }
+    },function(err) {
+        if (err) { console.log(err); return }
+        Quagga.initialized = true;
+        Quagga.start();
+    });
+
+  }
+};
+//End of quagga functions
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   // fetchBooks();
   fetchGoogleBooks();
+  load_quagga();
   console.log("hello");
 });
